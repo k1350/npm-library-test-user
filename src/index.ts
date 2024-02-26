3b3bf941abd5b16ab3844d8ed7b6c1913e6dfc7a
Kimiko Mitamura <pom96egran46ate@gmail.com>
2024-02-26 21:14:05 +0900

    0.1.1-3

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { sum } from "@k1350/npm-library-test/test";
 import { TEST, type TestType } from "@k1350/npm-library-test/test2";
+import { Test3Child } from "@k1350/npm-library-test/test3/child";
 
 export function HelloWorld() {
     const result = sum(4, 2);
@@ -7,6 +8,7 @@ export function HelloWorld() {
     console.log(TEST);
     const test: TestType = "3";
     console.log('Hello World', test)
+    console.log( Test3Child());
 }
 
 HelloWorld();
